22fe05f24b9ae692a491810a4f076d24696b6da2
Yukino Ikegami <yknikgm@gmail.com>
2019-03-01 02:13:41 +0900

    Update Python related files

--- python/test.py
+++ python/test.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-
 import CaboCha
 
-# c = CaboCha.Parser("");
 c = CaboCha.Parser()
 
 sentence = "太郎はこの本を二郎を見た女性に渡した。"
 
-print c.parseToString(sentence)
+print(c.parseToString(sentence))
 
 tree =  c.parse(sentence)
 
-print tree.toString(CaboCha.FORMAT_TREE)
-print tree.toString(CaboCha.FORMAT_LATTICE)
+print(tree.toString(CaboCha.FORMAT_TREE))
+print(tree.toString(CaboCha.FORMAT_LATTICE))
