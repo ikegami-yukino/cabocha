21448a71a4e7d089a8537d8bed6f41bc46657bdb
Yukino Ikegami <yknikgm@gmail.com>
2019-09-21 02:40:07 +0900

    Avoid the error 'Python.h' file not found + autopep8

--- python/setup.py
+++ python/setup.py
@@ -14,6 +14,12 @@ def cmd2(str):
     return cmd1(str).split()
 
 
+def get_python_inc_dir():
+    # For the error 'Python.h' file not found
+    includes = cmd2('python-config --includes')
+    return [include[2:] for include in includes]  # for removing "-I"
+
+
 if platform.system() == 'Windows':
     if sys.maxsize > 2**32:  # 64bit
         raise WindowsError('64bit Python for Windows is not supported')
@@ -28,20 +34,21 @@ if platform.system() == 'Windows':
             )
         ]
         data_files = [('lib\\site-packages\\',
-            ["C:\Program Files (x86)\CaboCha\\bin\libcabocha.dll",
-             "C:\Program Files (x86)\CaboCha\\bin\libcrfpp.dll"])]
+                       ["C:\Program Files (x86)\CaboCha\\bin\libcabocha.dll",
+                        "C:\Program Files (x86)\CaboCha\\bin\libcrfpp.dll"])]
 else:
     ext_modules = [
-          Extension("_CaboCha",
-                    ["CaboCha_wrap.cxx"],
-                    include_dirs=cmd2("cabocha-config --inc-dir"),
-                    library_dirs=cmd2("cabocha-config --libs-only-L"),
-                    libraries=cmd2("cabocha-config --libs-only-l"))
+        Extension("_CaboCha",
+                  ["CaboCha_wrap.cxx"],
+                  include_dirs=cmd2("cabocha-config --inc-dir") +
+                  get_python_inc_dir(),
+                  library_dirs=cmd2("cabocha-config --libs-only-L"),
+                  libraries=cmd2("cabocha-config --libs-only-l"))
     ]
     data_files = None
 
 setup(name="cabocha-python",
-      version='0.69',
+      version='0.69.1',
       py_modules=["CaboCha"],
       ext_modules=ext_modules,
       data_files=data_files,
@@ -51,18 +58,18 @@ setup(name="cabocha-python",
       license='BSD or LGPL',
       platforms=['Windows', 'macOS', 'Linux'],
       classifiers=[
-        'Development Status :: 3 - Alpha',
-        'License :: OSI Approved :: BSD License',
-        'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',
-        'Natural Language :: Japanese',
-        'Intended Audience :: Science/Research',
-        'Operating System :: Microsoft :: Windows :: Windows 10',
-        'Operating System :: MacOS :: MacOS X',
-        'Operating System :: POSIX :: Linux',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Topic :: Text Processing'
+          'Development Status :: 3 - Alpha',
+          'License :: OSI Approved :: BSD License',
+          'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',
+          'Natural Language :: Japanese',
+          'Intended Audience :: Science/Research',
+          'Operating System :: Microsoft :: Windows :: Windows 10',
+          'Operating System :: MacOS :: MacOS X',
+          'Operating System :: POSIX :: Linux',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
+          'Topic :: Text Processing'
       ],
       description='Python wrapper for CaboCha: Japanese Dependency Structure Analyzer',
       long_description='''This is a python wrapper for CaboCha.
@@ -71,8 +78,6 @@ setup(name="cabocha-python",
 
     License
     ---------
-    CaboCha is copyrighted free software by Taku Kudo <taku@chasen.org>
-is released under any of the the LGPL (see the file LGPL) or the
-BSD License (see the file BSD).
+    CaboCha is copyrighted free software by Taku Kudo <taku@chasen.org> is released under any of the the LGPL or the BSD License.
     '''
-)
+      )
